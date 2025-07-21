a119772f84ecb0177ea3c040c575946aecb688e7
Edanriell <Edanriell@gmail.com>
2025-07-21 23:01:00 +0300

    bugfix(minor-fixes): minor fixes

---
+++ News/Services/ISettingsService.cs
@@ -0,0 +1,18 @@
+﻿namespace News.Services;
+
+public interface ISettingsService
+{
+    double FontSize { get; set; }
+    bool DarkMode { get; set; }
+    bool NotificationsEnabled { get; set; }
+    bool AutoRefresh { get; set; }
+    int RefreshInterval { get; set; }
+
+    event EventHandler<double> FontSizeChanged;
+    event EventHandler<bool> DarkModeChanged;
+    event EventHandler<bool> AutoRefreshChanged;
+
+    void LoadSettings();
+    void SaveSettings();
+    void ResetToDefaults();
+}
